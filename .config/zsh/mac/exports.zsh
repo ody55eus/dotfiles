51622fdf564ea88cedb4ac8c27b3d3311b6609e6
Jonathan Pieper <ganymede+github@mailbox.org>
2021-07-28 07:46:43 +0200

    added mac config

---
+++ .config/zsh/mac/exports.zsh
@@ -0,0 +1,20 @@
+export PATH
+eval $(/opt/homebrew/bin/brew shellenv)
+
+# Prefer US English and use UTF-8.
+export LANG='en_US.UTF-8';
+export LC_ALL='en_US.UTF-8';
+
+# Highlight section titles in manual pages.
+export LESS_TERMCAP_md="${yellow}";
+
+# Don’t clear the screen after quitting a manual page.
+# export MANPAGER='less -X';
+
+# Avoid issues with `gpg` as installed via Homebrew.
+# https://stackoverflow.com/a/42265848/96656
+export GPG_TTY=$(tty);
+
+# Hide the “default interactive shell is now zsh” warning on macOS.
+export BASH_SILENCE_DEPRECATION_WARNING=1;
+
