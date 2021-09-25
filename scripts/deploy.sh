bf5fd2743ef351804a3d928c42cad34e57b9b3e6
Jonathan Pieper <ody55eus@mailbox.org>
2021-09-25 18:08:48 +0200

    Updated Deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# UPDATE="sudo pacman -Syu"
-# INSTALL="sudo pacman -S"
-
 # Yay has more packages
 UPDATE="yay -Syu"
 INSTALL="yay -S"
 
-#UPDATE="apt update && apt upgrade -y"
-#INSTALL="apt install"
-
-git submodule update --init sh/.config/zsh/ohmyzsh
-git submodule update --init doom/.emacs.d
-git submodule update --init vim/.vim/bundle/Vundle.vim
+# Alternatively use pacman
+# UPDATE="sudo pacman -Syu"
+# INSTALL="sudo pacman -S"
 
 $UPDATE
 $INSTALL git neovim wget tmux stow
 
+$INSTALL conky
+
+$INSTALL rofi dmenu
+
+# Download OhMyZsh from github
+git submodule update --init sh/.config/zsh/ohmyzsh
 # Install OhMyZsh! Theme
 rm -rf sh/.config/zsh/ohmyzsh/custom/themes
 git clone https://github.com/romkatv/powerlevel10k.git sh/.config/zsh/ohmyzsh/custom/themes
@@ -31,4 +31,7 @@ make -j`$(nproc)`
 sudo make install
 cd -
 
+# Download Doom from Git
+git submodule update --init doom/.emacs.d
+# Install Doom
 doom/.emacs.d/bin/doom install
