f8cfe5ee1ab9a3680af0f7c848fc4e571992c6f9
Jonathan Pieper <ody55eus@mailbox.org>
2021-11-05 18:11:55 +0100

    updated shell variables

--- sh/.config/zsh/mac/exports.zsh
+++ sh/.config/zsh/mac/exports.zsh
@@ -2,7 +2,7 @@
 eval $(/opt/homebrew/bin/brew shellenv)
 
 # Add my own install path on MacOS
-export PATH=/opt/jp/bin:$PATH
+# export PATH=/opt/jp/bin:$PATH
 
 # Prefer US English and use UTF-8.
 export LANG='en_US.UTF-8';
@@ -11,6 +11,8 @@ export LC_ALL='en_US.UTF-8';
 # Highlight section titles in manual pages.
 # export LESS_TERMCAP_md="${yellow}";
 
+# Make vi (neovim) the default manpage viewer
+export MANPAGER="/bin/sh -c \"col -b | vi -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
 # Don’t clear the screen after quitting a manual page.
 # export MANPAGER='less -X';
 
@@ -22,18 +24,3 @@ export GPG_TTY=$(tty);
 export BASH_SILENCE_DEPRECATION_WARNING=1;
 
 # zsh_add_file '/opt/homebrew/Cellar/docker/20.10.8/share/zsh/site-functions/_docker'
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/homebrew/Caskroom/miniforge/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
-        . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
