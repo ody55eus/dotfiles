4ad4eb187d7ae07758d742592db3bf968503800a
Jonathan Pieper <ody55eus@mailbox.org>
2021-11-05 18:10:29 +0100

    removed resize hook

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -248,26 +248,6 @@
 (map! :n [mouse-8] #'better-jumper-jump-backward
       :n [mouse-9] #'better-jumper-jump-forward)
 
-(defun jp/set-frame-size-according-to-resolution ()
-  (interactive)
-  (if window-system
-  (progn
-    ;; use 120 char wide window for largeish displays
-    ;; and smaller 80 column windows for smaller displays
-    ;; pick whatever numbers make sense for you
-    (if (> (x-display-pixel-width) 1280)
-           (add-to-list 'default-frame-alist (cons 'width 177))
-           (add-to-list 'default-frame-alist (cons 'width 100)))
-    ;; for the height, subtract a couple hundred pixels
-    ;; from the screen height (for panels, menubars and
-    ;; whatnot), then divide by the height of a char to
-    ;; get the height we want
-    (add-to-list 'default-frame-alist
-         (cons 'height (/ (- (x-display-pixel-height) 120)
-                             (frame-char-height)))))))
-
-(jp/set-frame-size-according-to-resolution)
-
 (setq confirm-kill-emacs nil)           ;; Don't confirm every kill
 
 (setq
